62b295f9b8c0bd451cae099ea8d2a447cb9f3123
estefania-git <ecasmed@gmail.com>
2020-01-10 12:55:02 +0100

    added put

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,41 +4,11 @@ const bodyParser = require("body-parser");
 
 const User = require("../models/User.model");
 
-const read = (req, res, next) => {
-  const userId = req.params.id;
-  if (!userId) {
-    return res.status(400).json({
-      messaage: "Invalid user id"
-    });
-  }
-  User.findById(userId).exec((err, user) => {
-    if (err) {
-      return res.status(404).json({
-        message: "User not found: " + err.message
-      });
-    }
-    if (!user) {
-      return res.status(400).json({
-        message: "Invalid user id: " + userId
-      });
-    }
-
-    let usr = {};
-    usr._id = user._id;
-    usr.name = user.name;
-    usr.email = user.email;
-    usr.birthDates = user.birthDate;
-    usr.address = user.address;
-    return res.status(200).json(usr);
-  });
-};
-
 // router.get("/:id", (req, res, next) => {
 //   res.json({ mensaje: "estoy en users" });
 // });
 
 router.get("/getusers", (req, res, next) => {
-  // const { id, name, email, birthDate, address } = req.body;
   User.find()
     .then(user => {
       console.log(user.data);
@@ -110,14 +80,11 @@ router.get("/getusersById/:id", (req, res, next) => {
 router.put("/updateUsersById/:id", (req, res, next) => {
   const { id } = req.params;
   User.findByIdAndUpdate(id, { name: "carolina", email: "carolina@hotmail.es" })
-    .then(() => {
-      return res
-        .status(200)
-        .json(user)
-        .stringify({
-          message: "OK"
-        });
-    })
+    .then(() =>
+      res.status(200).json({
+        message: "OK"
+      })
+    )
     .catch(error =>
       res.status(400).json({
         message: "Invalid user id"
